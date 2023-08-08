b4bf4739088547e951f1291f8a8505a3ab8dd636
William <whiranpat2996@gmail.com>
2023-08-08 19:38:58 -0400

    Resolved issue of being unable to exit projectlist

--- LandingPage/urls.py
+++ LandingPage/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from.views import view
+from . views import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view),
     path('index/', view, name='index'),
     path('index/projects/', include('projectList.urls'))
 ]
